a22cfad0a3257d1346fa38f0522627a48ab4bc3e
slim13009 <108020126+slim13009@users.noreply.github.com>
2022-08-14 16:22:00 +0200

    Mise en place
    Ajouts éléments codes et scripts pour le projet

---
+++ script_projet.js
@@ -0,0 +1,34 @@
+//BODY SELECTIONNER
+let corps = d3.select ('body');
+    
+//CREER LE CANEVAS    
+let monsvg= corps.append ('svg')
+  .attr('width',400)
+  .attr('height',400);
+//ajouter le cercle
+let cercle = monsvg.append ('circle')
+   .attr('cx',140)
+   .attr('cy',70)
+   .attr('r',40)
+   .style('fill','red');
+  
+let ligne = monsvg
+.append ('polyline')
+.attr("points",'20,20,60,100,90,20,200,20')
+.style("stroke",'purple')
+.style("fill",'none')
+.style("stroke-width",5)
+;
+
+let losage = monsvg
+.append('polygon')
+.attr("points","141,30,101,70,141,110,180,70,")
+.style("stroke","blue")
+.style("stroke-width",1)
+.style("fill","blue")
+let losage2 = monsvg
+.append('polygon')
+.attr("points","141,30,101,70,180,70,141,110")
+.style("stroke","orange")
+.style("stroke-width",1)
+.style("fill","orange")
